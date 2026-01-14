11cecd46e37e643a750e5a431beb21e1cac09aaf
Riley Hales PhD <39097632+rileyhales@users.noreply.github.com>
2026-01-14 14:51:00 -0700

    use imports and modules

--- datasets/convert_csv_to_geojson.py
+++ datasets/convert_csv_to_geojson.py
@@ -1,26 +1,35 @@
 import pandas as pd
 import geopandas as gpd
+import json
+
 
 df = pd.read_csv('./lowheaddams.csv')
 df = df[[
     'name',
-    'city',
-    'county',
-    'state',
+    # 'city',
+    # 'county',
+    # 'state',
     'latitude',
     'longitude',
     'linkno',
-    'reach_id',
+    # 'reach_id',
     'num_fatalities',
     'Qmin',
     'Qmax',
-]]
-(
+]].rename(columns={
+    'num_fatalities': 'deaths',
+    'linkno': 'river',
+})
+
+geojson = (
     gpd
     .GeoDataFrame(
         df.drop(columns=['latitude', 'longitude']),
         geometry=gpd.points_from_xy(df.longitude, df.latitude)
     )
     .set_crs(epsg=4326)
-    .to_file('../public/lowheaddams.geojson', driver='GeoJSON')
+    .to_json()
 )
+
+with open('../public/lowheaddams.geojson', "w") as f:
+    json.dump(json.loads(geojson), f, separators=(",", ":"))
--- src/main.js
+++ src/main.js
@@ -1,48 +1,56 @@
-require(
-  // naad mapimagelayer and geojsonlayer
-  ["esri/layers/MapImageLayer", "esri/layers/GeoJSONLayer"],
-  (MapImageLayer,  GeoJSONLayer) => {
-    document.querySelector('arcgis-map').addEventListener('arcgisViewReadyChange', () => {
-      const mapContainer = document.querySelector('arcgis-map')
-      const map = mapContainer.map
+import "@arcgis/core/assets/esri/themes/light/main.css"
+import "@arcgis/map-components/components/arcgis-map"
+import "@arcgis/map-components/components/arcgis-zoom"
+import "@arcgis/map-components/components/arcgis-layer-list"
+import "@arcgis/map-components/components/arcgis-locate"
+import "@arcgis/map-components/components/arcgis-scale-bar"
+import "@arcgis/map-components/components/arcgis-expand"
+import "@arcgis/map-components/components/arcgis-basemap-gallery"
 
-      const rfsLayer = new MapImageLayer({
-        url: 'https://livefeeds3.arcgis.com/arcgis/rest/services/GEOGLOWS/GlobalWaterModel_Medium/MapServer',
-        title: "GEOGLOWS River Forecast System v2",
-        sublayers: [{id: 0, definitionExpression: "rivercountry='United States' OR outletcountry='United States'"}]
-      })
+import MapImageLayer from "@arcgis/core/layers/MapImageLayer"
+import GeoJSONLayer from "@arcgis/core/layers/GeoJSONLayer"
 
-      // add the lowheaddam.geojson layer from the public folder
-      const lowheadDamsLayer = new GeoJSONLayer({
-        url: '/lowheaddams.geojson',
-        title: 'Low Head Dams',
-        renderer: {
-          type: "simple",
-          symbol: {
-            type: "simple-marker",
-            style: "triangle",
-            color: "red",
-            size: "12px",
-            outline: {
-              color: "white",
-              width: 1
-            }
-          }
-        },
-        popupTemplate: {
-          title: "{name}",
-          content: "" +
-            "<b>Dam Name:</b> {name}<br>" +
-            "<b>City</b>: {city}<br>" +
-            "<b>County</b>: {county}<br>" +
-            "<b>State:</b> {state}<br>" +
-            "<b>Known Fatalities</b>: {num_fatalities}<br>" +
-            "<b>Minimum Discharge for Hydraulic Jump</b>: {Qmin} m<sup>3</sup>/s<br>" +
-            "<b>Maximum Discharge for Hydraulic Jump</b>: {Qmax} m<sup>3</sup>/s<br>"
+import './main.css'
+
+document.querySelector('arcgis-map').addEventListener('arcgisViewReadyChange', () => {
+  const mapContainer = document.querySelector('arcgis-map')
+  const map = mapContainer.map
+
+  const rfsLayer = new MapImageLayer({
+    url: 'https://livefeeds3.arcgis.com/arcgis/rest/services/GEOGLOWS/GlobalWaterModel_Medium/MapServer',
+    title: "GEOGLOWS River Forecast System v2",
+    sublayers: [{id: 0, definitionExpression: "rivercountry='United States' OR outletcountry='United States'"}]
+  })
+
+  const lowheadDamsLayer = new GeoJSONLayer({
+    // url: new URL('/lowheaddams.geojson', import.meta.url).href,
+    url: '/lowheaddams.geojson',
+    title: 'Low Head Dams',
+    renderer: {
+      type: "simple",
+      symbol: {
+        type: "simple-marker",
+        style: "triangle",
+        color: "red",
+        size: "12px",
+        outline: {
+          color: "white",
+          width: 1
         }
-      })
+      }
+    },
+    popupTemplate: {
+      title: "{name}",
+      content: "" +
+        "<b>Dam Name:</b> {name}<br>" +
+        "<b>City</b>: {city}<br>" +
+        "<b>County</b>: {county}<br>" +
+        "<b>State:</b> {state}<br>" +
+        "<b>Known Fatalities</b>: {num_fatalities}<br>" +
+        "<b>Minimum Discharge for Hydraulic Jump</b>: {Qmin} m<sup>3</sup>/s<br>" +
+        "<b>Maximum Discharge for Hydraulic Jump</b>: {Qmax} m<sup>3</sup>/s<br>"
+    }
+  })
 
-      map.addMany([rfsLayer, lowheadDamsLayer])
-    })
-  }
-)
+  map.addMany([rfsLayer, lowheadDamsLayer])
+})
